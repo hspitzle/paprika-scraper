b8ecb879785e7d8e3a606e0c8e8f0fc02a4f81de
Hayden Spitzley <hspitzle@umich.edu>
2020-03-22 10:26:17 -0400

    alert on error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import formatCsv from './formatter';
-import sendMail from './mailer';
+import sendEmail from './mailer';
 import fetchRecipes from './scraper';
 
 const run = async (request: any, response: any) => {
   const recipes = await fetchRecipes();
   const csv = formatCsv(recipes);
-  await sendMail(csv);
+  try {
+    await sendEmail(csv);
+  } catch(err) {
+    console.log('Mailgun email failed:', err.message);
+  }
   return response ? response.status(200).send('Recipes scraped and sent!') : 'complete';
 };
 
